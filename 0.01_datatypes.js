a13e2d02a65225c6744feb1ff18146ba07be6e0b
Christian Ingram <clingram1@aol.com>
2017-08-16 13:13:21 -0400

    datatypes

---
+++ 0.01_datatypes.js
@@ -0,0 +1,63 @@
+// DataTypes
+
+// Strings
+// 
+"What a Wonderful world";
+'Hello you guys';
+
+"'it's a wonderful day'"
+
+console.log('Thomas said "I think grass is green!"'.toUpperCase());
+
+console.log('Couldn\'t things get any better'.length)
+
+
+
+// Numbers
+
+console.log(typeof(2));
+console.log(2+5) //7
+console.log(2-5)
+console.log(2/5)
+console.log(2*5)
+console.log(10%3)
+
+// Booleans
+
+console.log(true)
+/*
+	Conditional Operators
+	<  : less than
+	>  :greater than
+	<=	:less than or equal to
+	>=	:greater than or equal to
+	==	:equal to
+	===	:strictly equal to
+	/	:not
+	/=	:not equal to
+*/
+
+console.log(4 == 5);
+console.log(1 =="1");
+console.log(1 ==="1");
+
+
+
+
+
+
+
+
+//Challenge create a string that has your name and use the to lowercase function to  make 
+//sure each character is lower case
+
+console.log('Christian Ingram'.toLowerCase());
+console.log('Christian'[0].toUpperCase());
+console.log('Christian'[0].toUpperCase() + 'Christian'[1]+ 'Christian'[2]);
+
+//Challenge uppercase the 'm' in 'Home' and put the string
+//back together using string concatenation
+console.log('Home'[0] + 'Home'[1] + 'Home'[2].toUpperCase() +'Home'[3]);
+
+console.log(typeof("Amber"));
+
