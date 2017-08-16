7ddc34ad327ddcebc57c07316388daf68eff84bb
Christian Ingram <clingram1@aol.com>
2017-08-16 14:09:44 -0400

    variables

---
+++ 0.02_variables.js
@@ -0,0 +1,51 @@
+// Variables
+
+var name;
+
+name = "Chistian";
+
+console.log (typeof(name));
+console.log(name)
+
+var lastName = "Ingram";
+var firstName = "Christian";
+var fullName ="Christian" +" "+ "Ingram"
+console.log(name.length)
+console.log(name.toUpperCase())
+
+var bd = "31";
+var year = "1976";
+var mbd = "512";
+var dbd = "69";
+var bbd = "14";
+
+console.log(mbd);
+
+var myNumber = 0
+console.log(myNumber)
+myNumber = (myNumber+10);
+console.log(myNumber);
+myNumber = (myNumber-1);
+console.log(myNumber)
+myNumber =(myNumber*9);
+console.log(myNumber)
+
+
+
+
+// Variable data types
+
+
+//Challenge create 2 variables 'firstName' and 'lastName' and concat them
+//together in a third variable called 'fullName' and pring fullName to the console
+
+console.log(lastName);
+console.log(fullName);
+
+
+
+
+
+// Variable scopes
+
+
